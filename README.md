40be6880baf449c9e579c2a12bef7561c1bbde72
Mark Gong-Guy <mark.a.gongguy@gmail.com>
2017-03-26 17:49:53 -0700

    File Structure and Config File

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BiolaCSCI
 Remaking the outdated Biola University CSCI website
+
+
+
+Directory Structure:
+
+img — All your image files.
+css — All your css files.
+js — All your javascript files.
+config.php — Main configuration file. Should store site wide settings.
+library — Central location for all custom and third party libraries.
+templates — Reusable components that make up your layout.
---
+++ resources/config.php
@@ -0,0 +1,29 @@
+<?php
+
+/* Biola CS Site Configuration File
+   Returns: Array of Configuration Variables
+   Use:
+
+   <?php
+   $config = include('resources/config.php');
+   echo $database['dbhost']; // 'localhost'
+   ?>
+
+
+ */
+
+return [
+
+/* Database Configuration Settings */
+  'dbhost' => 'localhost',
+  'dbuser' => 'dbuser',
+  'dbpassword' => 'dbpassword',
+
+/* Debug Configuration Settings */
+  ini_set("error_reporting", "true"); //Enable or disable error reporting
+  error_reporting(E_ALL|E_STRCT);
+
+];
+
+
+ ?>
