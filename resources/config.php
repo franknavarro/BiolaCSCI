d92d91dbf4c89daa6e11f8455b3ec7b8eefa72f4
Mark Gong-Guy <mark.a.gongguy@gmail.com>
2017-04-03 17:04:52 -0700

    Formatting Change

--- resources/config.php
+++ resources/config.php
@@ -1,30 +1,30 @@
 <?php
 
 /* Biola CS Site Configuration File
-   Returns: Array of Configuration Variables
-   Use:
+Returns: Array of Configuration Variables
+Use:
 
-   <?php
-   $config = include('resources/config.php');
-   echo $database['dbhost']; // 'localhost'
-   ?>
+<?php
+$config = include('resources/config.php');
+echo $database['dbhost']; // 'localhost'
+?>
 
 
- */
+*/
 
 return [
 
-/* Database Configuration Settings */
-  'dbhost' => 'localhost',
-  'dbname' => 'dbname',
-  'dbuser' => 'dbuser',
-  'dbpassword' => 'dbpassword',
+    /* Database Configuration Settings */
+    'dbhost' => 'localhost',
+    'dbname' => 'dev_csweb',
+    'dbuser' => 'dev_user',
+    'dbpassword' => '=hemagu2u@Eb',
 
-/* Debug Configuration Settings */
-  ini_set("error_reporting", "true"); //Enable or disable error reporting
-  error_reporting(E_ALL|E_STRCT);
+    /* Debug Configuration Settings */
+    ini_set("error_reporting", "true"); //Enable or disable error reporting
+    error_reporting(E_ALL|E_STRCT);
 
 ];
 
 
- ?>
+?>
