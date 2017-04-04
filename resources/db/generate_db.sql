767d10200ed315cadf84e8a1e6fd4d1f31415517
pattpattpatt <alex.j.patton@biola.edu>
2017-04-04 12:49:32 -0700

    Updated db creation script
    changed schema name to "cscidb" and fixed a data type error.

--- resources/db/generate_db.sql
+++ resources/db/generate_db.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema cscidb
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema cscidb
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `cscidb` DEFAULT CHARACTER SET utf8 ;
+USE `cscidb` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`user`
+-- Table `cscidb`.`user`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`user` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`user` (
   `email` VARCHAR(45) NOT NULL,
   `firstName` VARCHAR(45) NOT NULL,
   `lastName` VARCHAR(45) NOT NULL,
@@ -30,9 +30,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`log_login`
+-- Table `cscidb`.`log_login`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`log_login` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`log_login` (
   `loginID` INT NOT NULL AUTO_INCREMENT,
   `timestamp` DATETIME NOT NULL,
   `incorrectIP` VARCHAR(45) NOT NULL,
@@ -41,16 +41,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`log_login` (
   INDEX `fk_log_login_user1_idx` (`user_email` ASC),
   CONSTRAINT `fk_log_login_user1`
     FOREIGN KEY (`user_email`)
-    REFERENCES `mydb`.`user` (`email`)
+    REFERENCES `cscidb`.`user` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`session`
+-- Table `cscidb`.`session`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`session` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`session` (
   `sessionID` INT NOT NULL AUTO_INCREMENT,
   `hostID` INT UNSIGNED NOT NULL,
   `sessionName` VARCHAR(45) NOT NULL,
@@ -61,9 +61,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`class`
+-- Table `cscidb`.`class`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`class` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`class` (
   `classID` INT NOT NULL AUTO_INCREMENT,
   `professorID` INT UNSIGNED NOT NULL,
   `className` VARCHAR(45) NULL,
@@ -78,16 +78,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`class` (
   INDEX `fk_class_session1_idx` (`lastSessionID` ASC),
   CONSTRAINT `fk_class_session1`
     FOREIGN KEY (`lastSessionID`)
-    REFERENCES `mydb`.`session` (`sessionID`)
+    REFERENCES `cscidb`.`session` (`sessionID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`question`
+-- Table `cscidb`.`question`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`question` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`question` (
   `questionID` INT NOT NULL AUTO_INCREMENT,
   `sessionID` INT NOT NULL,
   `questionBody` TEXT NOT NULL,
@@ -97,21 +97,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`question` (
   INDEX `fk_question_user1_idx` (`user_email` ASC),
   CONSTRAINT `fk_question_session1`
     FOREIGN KEY (`sessionID`)
-    REFERENCES `mydb`.`session` (`sessionID`)
+    REFERENCES `cscidb`.`session` (`sessionID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_question_user1`
     FOREIGN KEY (`user_email`)
-    REFERENCES `mydb`.`user` (`email`)
+    REFERENCES `cscidb`.`user` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`answer`
+-- Table `cscidb`.`answer`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`answer` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`answer` (
   `answerID` INT NOT NULL AUTO_INCREMENT,
   `questionID` INT NOT NULL,
   `sessionID` INT NOT NULL,
@@ -124,26 +124,26 @@ CREATE TABLE IF NOT EXISTS `mydb`.`answer` (
   INDEX `fk_answer_user1_idx` (`user_email` ASC),
   CONSTRAINT `fk_answer_question1`
     FOREIGN KEY (`questionID`)
-    REFERENCES `mydb`.`question` (`questionID`)
+    REFERENCES `cscidb`.`question` (`questionID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_answer_session1`
     FOREIGN KEY (`sessionID`)
-    REFERENCES `mydb`.`session` (`sessionID`)
+    REFERENCES `cscidb`.`session` (`sessionID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_answer_user1`
     FOREIGN KEY (`user_email`)
-    REFERENCES `mydb`.`user` (`email`)
+    REFERENCES `cscidb`.`user` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`attendance`
+-- Table `cscidb`.`attendance`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`attendance` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`attendance` (
   `attendanceID` INT NOT NULL AUTO_INCREMENT,
   `sessionID` INT NOT NULL,
   `state` TINYINT(1) NOT NULL,
@@ -153,24 +153,24 @@ CREATE TABLE IF NOT EXISTS `mydb`.`attendance` (
   INDEX `fk_attendance_user1_idx` (`user_email` ASC),
   CONSTRAINT `fk_attendance_session1`
     FOREIGN KEY (`sessionID`)
-    REFERENCES `mydb`.`session` (`sessionID`)
+    REFERENCES `cscidb`.`session` (`sessionID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_attendance_user1`
     FOREIGN KEY (`user_email`)
-    REFERENCES `mydb`.`user` (`email`)
+    REFERENCES `cscidb`.`user` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`files`
+-- Table `cscidb`.`files`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`files` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`files` (
   `FileName` VARCHAR(45) NOT NULL,
   `FileText` BLOB NULL,
-  `UpdateTime` DATETIME(8) NULL,
+  `UpdateTime` DATETIME NULL,
   `FileSize` INT NULL,
   `ParentFolderID` VARCHAR(45) NULL,
   `user_email` VARCHAR(45) NOT NULL,
@@ -178,16 +178,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`files` (
   INDEX `fk_Files_user1_idx` (`user_email` ASC),
   CONSTRAINT `fk_Files_user1`
     FOREIGN KEY (`user_email`)
-    REFERENCES `mydb`.`user` (`email`)
+    REFERENCES `cscidb`.`user` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`folder`
+-- Table `cscidb`.`folder`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`folder` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`folder` (
   `FolderID` INT NOT NULL,
   `FolderName` VARCHAR(45) NULL,
   `ParentFolderID` VARCHAR(45) NULL,
@@ -200,36 +200,36 @@ CREATE TABLE IF NOT EXISTS `mydb`.`folder` (
   INDEX `fk_Folder_user1_idx` (`user_email` ASC),
   CONSTRAINT `fk_Folder_Files1`
     FOREIGN KEY (`Files_FileName`)
-    REFERENCES `mydb`.`files` (`FileName`)
+    REFERENCES `cscidb`.`files` (`FileName`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Folder_user1`
     FOREIGN KEY (`user_email`)
-    REFERENCES `mydb`.`user` (`email`)
+    REFERENCES `cscidb`.`user` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Folder_has_Folder`
+-- Table `cscidb`.`Folder_has_Folder`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Folder_has_Folder` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`Folder_has_Folder` (
   `Folder_FolderID1` INT NOT NULL,
   PRIMARY KEY (`Folder_FolderID1`),
   INDEX `fk_Folder_has_Folder_Folder2_idx` (`Folder_FolderID1` ASC),
   CONSTRAINT `fk_Folder_has_Folder_Folder2`
     FOREIGN KEY (`Folder_FolderID1`)
-    REFERENCES `mydb`.`folder` (`FolderID`)
+    REFERENCES `cscidb`.`folder` (`FolderID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Folder_has_Files`
+-- Table `cscidb`.`Folder_has_Files`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Folder_has_Files` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`Folder_has_Files` (
   `Folder_FolderID` INT NOT NULL,
   `Files_FileName` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`Folder_FolderID`, `Files_FileName`),
@@ -237,21 +237,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Folder_has_Files` (
   INDEX `fk_Folder_has_Files_Folder1_idx` (`Folder_FolderID` ASC),
   CONSTRAINT `fk_Folder_has_Files_Folder1`
     FOREIGN KEY (`Folder_FolderID`)
-    REFERENCES `mydb`.`folder` (`FolderID`)
+    REFERENCES `cscidb`.`folder` (`FolderID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Folder_has_Files_Files1`
     FOREIGN KEY (`Files_FileName`)
-    REFERENCES `mydb`.`files` (`FileName`)
+    REFERENCES `cscidb`.`files` (`FileName`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`channels`
+-- Table `cscidb`.`channels`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`channels` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`channels` (
   `ChannelID` INT NOT NULL,
   `MessageID` INT NULL,
   `CreatorID` INT NULL,
@@ -260,16 +260,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`channels` (
   INDEX `fk_Channels_user1_idx` (`user_email` ASC),
   CONSTRAINT `fk_Channels_user1`
     FOREIGN KEY (`user_email`)
-    REFERENCES `mydb`.`user` (`email`)
+    REFERENCES `cscidb`.`user` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`messages`
+-- Table `cscidb`.`messages`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`messages` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`messages` (
   `MessageID` INT NOT NULL,
   `postTime` DATETIME NULL,
   `Message` LONGTEXT NULL,
@@ -280,21 +280,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`messages` (
   INDEX `fk_Messages_user1_idx` (`posterID` ASC),
   CONSTRAINT `fk_Messages_Channels1`
     FOREIGN KEY (`Channels_ChannelID`)
-    REFERENCES `mydb`.`channels` (`ChannelID`)
+    REFERENCES `cscidb`.`channels` (`ChannelID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_Messages_user1`
     FOREIGN KEY (`posterID`)
-    REFERENCES `mydb`.`user` (`email`)
+    REFERENCES `cscidb`.`user` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`user_class`
+-- Table `cscidb`.`user_class`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`user_class` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`user_class` (
   `iduser_class` INT NOT NULL,
   `role` VARCHAR(45) NULL,
   `user_email` VARCHAR(45) NOT NULL,
@@ -304,21 +304,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`user_class` (
   INDEX `fk_user_class_class1_idx` (`class_classID` ASC),
   CONSTRAINT `fk_user_class_user1`
     FOREIGN KEY (`user_email`)
-    REFERENCES `mydb`.`user` (`email`)
+    REFERENCES `cscidb`.`user` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_user_class_class1`
     FOREIGN KEY (`class_classID`)
-    REFERENCES `mydb`.`class` (`classID`)
+    REFERENCES `cscidb`.`class` (`classID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`assignment`
+-- Table `cscidb`.`assignment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`assignment` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`assignment` (
   `name` INT NOT NULL,
   `description` LONGTEXT NULL,
   `dueDate` DATETIME NULL,
@@ -331,16 +331,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`assignment` (
   INDEX `fk_assignment_class1_idx` (`class_classID` ASC),
   CONSTRAINT `fk_assignment_class1`
     FOREIGN KEY (`class_classID`)
-    REFERENCES `mydb`.`class` (`classID`)
+    REFERENCES `cscidb`.`class` (`classID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`announcement`
+-- Table `cscidb`.`announcement`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`announcement` (
+CREATE TABLE IF NOT EXISTS `cscidb`.`announcement` (
   `name` INT NOT NULL,
   `description` VARCHAR(45) NULL,
   `postTime` VARCHAR(45) NULL,
@@ -351,12 +351,12 @@ CREATE TABLE IF NOT EXISTS `mydb`.`announcement` (
   INDEX `fk_announcement_user1_idx` (`user_email` ASC),
   CONSTRAINT `fk_announcement_class1`
     FOREIGN KEY (`class_classID`)
-    REFERENCES `mydb`.`class` (`classID`)
+    REFERENCES `cscidb`.`class` (`classID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_announcement_user1`
     FOREIGN KEY (`user_email`)
-    REFERENCES `mydb`.`user` (`email`)
+    REFERENCES `cscidb`.`user` (`email`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
