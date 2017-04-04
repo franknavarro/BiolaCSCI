69b264bc614dd807c259179427df4ee105913310
peter-a-alford <peteralford13@gmail.com>
2017-04-04 16:05:44 -0700

    Class Object initial files

---
+++ process.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+ : <?php echo $_POST["name"]; ?><br>
+ : <?php echo $_POST["description"]; ?><br>
+ : <?php echo $_POST["syllabus"]; ?><br>
+ : <?php echo $_POST["assignments"]; ?><br>
+ : <?php echo $_POST["calendar"]; ?><br>
+ : <?php echo $_POST["announcement"]; ?><br>
+ : <?php echo $_POST["instructor"]; ?><br>
+ : <?php echo $_POST["classTime"]; ?><br>
+ : <?php echo $_POST["location"]; ?><br>
+
+</body>
+</html>
