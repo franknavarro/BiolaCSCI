6e249a33b89ffd8cda6b96fd20d4c467fb4ad5b0
Peter Alford <Mask@peters-air.biola.edu>
2017-04-05 17:10:23 -0700

    Update Class Object

---
+++ ClassObject/classProcess.php
@@ -0,0 +1,15 @@
+<html>
+    <body>
+         : <?php echo $_POST["name"]; ?><br>
+         : <?php echo $_POST["classID"]; ?><br>
+         : <?php echo $_POST["location"]; ?><br>
+         : <?php echo $_POST["classTime"]; ?><br>
+         : <?php echo $_POST["syllabusURL"]; ?><br>
+         : <?php echo $_POST["professorName"]; ?><br>
+         : <?php echo $_POST["professorEmail"]; ?><br>
+         : <?php echo $_POST["professorHours"]; ?><br>
+         : <?php echo $_POST["taName"]; ?><br>
+         : <?php echo $_POST["taEmail"]; ?><br>
+         : <?php echo $_POST["taHours"]; ?><br>
+    </body>
+</html>
