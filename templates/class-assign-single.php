2818bf82b51042dca57591e9d3819f0a9fcab0f9
Frank Navarro <franknavarro@users.noreply.github.com>
2017-04-25 00:09:06 -0700

    Finished Class Assignment Pages

---
+++ templates/class-assign-single.php
@@ -0,0 +1,8 @@
+<div id="assignment-single">
+    <h2 class="title">Title</h2>
+    <hr style="margin-top:10px;">
+    <h5><strong>Due </strong><span class="due-date">April 30, 2017</span></h5>
+    <h5><strong>URL/File: </strong><a class="url-file" target="_blank" href="http://heeeeeeeey.com/">Homework Reading Website</a></h5>
+    <hr>
+    <p class="assignment-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet quam lacinia, scelerisque lorem tempus, facilisis dolor. Vivamus elementum nisl at viverra blandit. Sed ultrices lacinia diam, vitae dapibus urna. Duis id purus viverra, vestibulum purus vitae, bibendum ex. Donec eleifend lobortis libero, vel malesuada nunc elementum vel. Phasellus elit ante, accumsan eu auctor quis, cursus vitae sapien. Aliquam malesuada erat enim, ut ullamcorper eros aliquam eget. Curabitur venenatis quam sit amet est bibendum ultricies.</p>
+</div>
