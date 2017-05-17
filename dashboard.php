8c6aaf399a2af9b82a97eded944d8da8de029560
Mark Gong-Guy <mark.a.gongguy@gmail.com>
2017-05-17 12:34:49 -0700

    Dashboard Functionality and Database Fix

--- dashboard.php
+++ dashboard.php
@@ -15,7 +15,7 @@ if(isset($_SESSION['user_id'])){
     <body>
         <div class="page-header" style="padding-left: 1em">
           <h1><center>CSCI Dashboard</center></br></br><small> Welcome, <?php echo $_SESSION['user_fName']; ?></small></h1>
-          <div class="row" style="padding-left: 3em">
+          <!-- <div class="row" style="padding-left: 3em">
             <div class="col-sm-6 col-md-4">
                 <div class="caption">
                   <h3>Introduction to Programming</h3>
@@ -25,26 +25,43 @@ if(isset($_SESSION['user_id'])){
                   </p>
                   <p><a href="#" class="btn btn-success" role="button">Join Session</a> <a href="#" class="btn btn-default" role="button">More Info</a></p>
                 </div>
-             </div>
-             <div class="col-sm-6 col-md-4">
-                 <div class="caption">
-                   <h3>Data Structures</h3>
-                   <p>
-                       Professor: Dr. Lin </br>
-                       Class Times: TR 10:30am - 12:00am
-                   </p>
-                   <p><a href="#" class="btn btn-danger" role="button">No Session</a> <a href="#" class="btn btn-default" role="button">More Info</a></p>
-                 </div>
-              </div>
-              <div class="col-sm-6 col-md-4">
-                  <div class="caption">
-                    <h3>Data Structures</h3>
-                    <p>
-                        Professor: Dr. Lin </br>
-                        Class Times: TR 10:30am - 12:00am
-                    </p>
-                    <p><a href="#" class="btn btn-danger" role="button">No Session</a> <a href="#" class="btn btn-default" role="button">More Info</a></p>
-                  </div>
+             </div> -->
+             <?php
+
+             include_once 'resources/library/db.php';
+             $class_id = db::query("SELECT class_classID from user_class where user_email = :user_email", array(':user_email'=>$_SESSION['user_id']));
+             for($count = 0; $count < count($class_id); $count++){
+
+                 $classID = print_r($class_id[$count]["class_classID"], true); //Variable to hold classID
+                 $result = db::query("SELECT className, syllabusURL, classTime, activeSession from class where classID = :classID", array(':classID'=>$classID));
+                 $className = print_r($result[0]["className"], true);
+                //  //Add Professor Email that links to First and Last Name
+                 $classTime = print_r($result[0]["classTime"], true);
+                 $activeSession = print_r($result[0]["activeSession"], true);
+                 $classURL = print_r($result[0]["syllabusURL"], true);
+
+                 echo '<div class="row" style="padding-left: 3em">';
+                 echo     '<div class="col-sm-6 col-md-4">';
+                 echo       '<div class="caption">';
+                 echo           "<h3>$className</h3>";
+                 echo               '<p>';
+                 echo                   'Professor: Dr. Lin </br>';
+                 echo                   "Class Times: $classTime";
+                 echo               '</p>';
+                 if($activeSession == 1){
+                     echo '<p><a href="#" class="btn btn-success" role="button">Join Session</a>';
+                 } else {
+                     echo '<p><a href="#" class="btn btn-danger" role="button">No Session</a>';
+                 }
+                 echo        '<a href="';
+                 echo        $classURL;
+                 echo        '" class="btn btn-default" role="button">More Info</a></p>';
+                 echo   '</div>';
+                 echo '</div>';
+
+
+             }
+              ?>
                </div>
         </div>
     </body>
--- resources/db/generate_db.sql
+++ resources/db/generate_db.sql
@@ -1,12 +1,6 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
-
--- -----------------------------------------------------
--- Schema cscidb
--- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema cscidb
@@ -69,20 +63,14 @@ CREATE TABLE IF NOT EXISTS `cscidb`.`class` (
   `professorEmail` VARCHAR (45) NULL,
   `taEmail` VARCHAR (45) NULL,
   `className` VARCHAR(45) NULL,
-  `lastSessionID` INT NOT NULL,
+  `activeSession` INT DEFAULT '0',
   `syllabusURL` VARCHAR(45) NULL,
   `classTime` VARCHAR(45) NULL,
   `room` VARCHAR(45) NULL,
   `taHours` VARCHAR(45) NULL,
   `instructorHours` VARCHAR(45) NULL,
   `classDescription` MEDIUMTEXT NULL,
-  PRIMARY KEY (`classID`),
-  INDEX `fk_class_session1_idx` (`lastSessionID` ASC),
-  CONSTRAINT `fk_class_session1`
-    FOREIGN KEY (`lastSessionID`)
-    REFERENCES `cscidb`.`session` (`sessionID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`classID`))
 ENGINE = InnoDB;
 
 
@@ -165,131 +153,6 @@ CREATE TABLE IF NOT EXISTS `cscidb`.`attendance` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-
--- -----------------------------------------------------
--- Table `cscidb`.`folder`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cscidb`.`folder` (
-  `folderID` INT NOT NULL,
-  `folderName` VARCHAR(45) NULL,
-  `parentFolderID` VARCHAR(45) NULL,
-  `childFileID` VARCHAR(45) NULL,
-  `user_email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`folderID`),
-  INDEX `fk_Folder_user1_idx` (`user_email` ASC),
-  CONSTRAINT `fk_Folder_user1`
-    FOREIGN KEY (`user_email`)
-    REFERENCES `cscidb`.`user` (`email`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cscidb`.`files`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cscidb`.`files` (
-  `fileName` VARCHAR(45) NOT NULL,
-  `fileBody` BLOB NULL,
-  `updateTime` DATETIME NULL,
-  `fileSize` FLOAT NULL,
-  `fileOwner` VARCHAR(45) NOT NULL,
-  `folder_folderID` INT NOT NULL,
-  PRIMARY KEY (`fileName`),
-  INDEX `fk_Files_user1_idx` (`fileOwner` ASC),
-  INDEX `fk_files_folder1_idx` (`folder_folderID` ASC),
-  CONSTRAINT `fk_Files_user1`
-    FOREIGN KEY (`fileOwner`)
-    REFERENCES `cscidb`.`user` (`email`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_files_folder1`
-    FOREIGN KEY (`folder_folderID`)
-    REFERENCES `cscidb`.`folder` (`folderID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cscidb`.`Folder_has_Folder`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cscidb`.`Folder_has_Folder` (
-  `Folder_FolderID1` INT NOT NULL,
-  PRIMARY KEY (`Folder_FolderID1`),
-  INDEX `fk_Folder_has_Folder_Folder2_idx` (`Folder_FolderID1` ASC),
-  CONSTRAINT `fk_Folder_has_Folder_Folder2`
-    FOREIGN KEY (`Folder_FolderID1`)
-    REFERENCES `cscidb`.`folder` (`folderID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cscidb`.`Folder_has_Files`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cscidb`.`Folder_has_Files` (
-  `Folder_FolderID` INT NOT NULL,
-  `Files_FileName` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Folder_FolderID`, `Files_FileName`),
-  INDEX `fk_Folder_has_Files_Files1_idx` (`Files_FileName` ASC),
-  INDEX `fk_Folder_has_Files_Folder1_idx` (`Folder_FolderID` ASC),
-  CONSTRAINT `fk_Folder_has_Files_Folder1`
-    FOREIGN KEY (`Folder_FolderID`)
-    REFERENCES `cscidb`.`folder` (`folderID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Folder_has_Files_Files1`
-    FOREIGN KEY (`Files_FileName`)
-    REFERENCES `cscidb`.`files` (`fileName`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cscidb`.`channels`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cscidb`.`channels` (
-  `channelID` INT NOT NULL,
-  `channel_name` VARCHAR(45) NULL,
-  `creatorID` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`channelID`),
-  INDEX `fk_Channels_user1_idx` (`creatorID` ASC),
-  CONSTRAINT `fk_Channels_user1`
-    FOREIGN KEY (`creatorID`)
-    REFERENCES `cscidb`.`user` (`email`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `cscidb`.`messages`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `cscidb`.`messages` (
-  `messageID` INT NOT NULL,
-  `postTime` DATETIME NULL,
-  `message` LONGTEXT NULL,
-  `posterID` VARCHAR(45) NOT NULL,
-  `channels_channelID` INT NOT NULL,
-  PRIMARY KEY (`messageID`),
-  INDEX `fk_Messages_user1_idx` (`posterID` ASC),
-  INDEX `fk_messages_channels1_idx` (`channels_channelID` ASC),
-  CONSTRAINT `fk_Messages_user1`
-    FOREIGN KEY (`posterID`)
-    REFERENCES `cscidb`.`user` (`email`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_messages_channels1`
-    FOREIGN KEY (`channels_channelID`)
-    REFERENCES `cscidb`.`channels` (`channelID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
 -- -----------------------------------------------------
 -- Table `cscidb`.`user_class`
 -- -----------------------------------------------------
@@ -363,9 +226,17 @@ CREATE TABLE IF NOT EXISTS `cscidb`.`announcement` (
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+-- Inserts Base Users
 INSERT INTO `cscidb`.`user` (`email`, `firstName`, `lastName`, `password`, `user_perm`) VALUES ('mark.a.gong-guy@biola.edu', 'Mark', 'Gong-Guy', 'password', '4');
 INSERT INTO `cscidb`.`user` (`email`, `firstName`, `lastName`, `password`, `user_perm`) VALUES ('peter.a.alford@biola.edu', 'Pedro', 'Wilson', 'password', '4');
 INSERT INTO `cscidb`.`user` (`email`, `firstName`, `lastName`, `password`, `user_perm`) VALUES ('peteralford13@gmail.com', 'Peter', 'Alford', 'password', '2');
+
+-- Inserts class
+INSERT INTO `cscidb`.`class` (`classID`, `classCode`, `className`, `syllabusURL`, `classTime`, `room`) VALUES ('1', 'CSCI 101', 'Introduction to Computer Science', 'http://csci.biola.edu/csci105Lin/index.htm', '10:00 am', 'Library 101');
+INSERT INTO `cscidb`.`class` (`classID`, `classCode`, `className`, `syllabusURL`, `classTime`, `room`) VALUES ('2', 'CSCI 102', 'Data Structures', 'http://csci.biola.edu/csci105Lin/index.htm', '10:00 am', 'Library 101');
+
+-- Inserts user in class
+INSERT INTO `cscidb`.`user_class` (`role`, `user_email`, `class_classID`) VALUES ('1', 'mark.a.gong-guy@biola.edu', '1');
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
