8615cb3ad07211e8914a11ea54afdcb311bd12c1
Tech Commons Patron <patron@a016445.biola.edu>
2017-05-17 23:16:27 -0700

    Revamped CodeCollab

--- js/code-collab.js
+++ js/code-collab.js
@@ -1,22 +1,65 @@
+/* Launch TogetherJS */
+$(document).ready(TogetherJS);
 
-window.onload = function() {
-    tabCapableText(); //Enabled Tabbed Text in textarea
-    TogetherJS(); //Run together JS
-    TogetherJS.config("dontShowClicks", true) //Disable Click Ping
+var textArea = document.getElementById("classroom-code");
+
+// Define an extended mixed-mode that understands vbscript and
+// leaves mustache/handlebars embedded templates in html mode
+var mixedMode = {
+    name: "htmlmixed",
+    scriptTypes: [{matches: /\/x-handlebars-template|\/x-mustache/i,
+                   mode: null},
+                  {matches: /(text|application)\/(x-)?vb(a|script)/i,
+                   mode: "vbscript"}]
 };
 
-//Makes the textarea tab-capable
-function tabCapableText() {
-    var textareas = document.getElementsByTagName('textarea');
-    var count = textareas.length;
-    for(var i=0;i<count;i++){
-        textareas[i].onkeydown = function(e){
-            if(e.keyCode==9 || e.which==9){
-                e.preventDefault();
-                var s = this.selectionStart;
-                this.value = this.value.substring(0,this.selectionStart) + "\t" + this.value.substring(this.selectionEnd);
-                this.selectionEnd = s+1;
-            }
-        }
-    }
+//Load up CodeMirror
+var myCodeMirror = CodeMirror.fromTextArea(textArea, {
+    value: "function myScript(){return 100;}\n",
+    mode:  "text/x-c++src",
+    theme: "default",
+    lineNumbers: true,
+    indentUnit: 4,
+    lineWrapping: true,
+    styleActiveLine: true,
+    autofocus: true,
+    matchBrackets: true,
+    autoCloseBrackets: true
+    
+});
+
+//Resize CodeMirror Page to fit the screen
+function resizeCodeMirror() {
+    var currentWindowHeight = $(window).height()-$("nav").height()-$("#langContainer").height();
+    var currentWindowWidth = $(window).width();
+    $(".CodeMirror").height(currentWindowHeight);
+    $(".CodeMirror").width(currentWindowWidth);
+    
+    $("#langSelector").width(currentWindowWidth-$("#langLabel").width());
 }
+
+//When file loads resize CodeMirror Screen
+resizeCodeMirror();
+
+//When window is resized resize all CodeMirror Screen
+$(window).resize(function() {
+    resizeCodeMirror();
+});
+
+//Change the coding language to what the user selected
+$("#langSelector").change(function() {
+    var newLanguage = $(this).val();
+    
+    if(newLanguage=="htmlmixed") {
+        myCodeMirror.setOption("mode", mixedMode);
+        myCodeMirror.setOption("matchTags", true);
+        myCodeMirror.setOption("autoCloseTags", true);
+    } else if (newLanguage=="application/x-httpd-php" || newLanguage=="django"){
+        myCodeMirror.setOption("matchTags", true);
+        myCodeMirror.setOption("autoCloseTags", true);
+    }else {
+        myCodeMirror.setOption("mode", newLanguage);
+        myCodeMirror.setOption("matchTags", false);
+        myCodeMirror.setOption("autoCloseTags", false);
+    }
+});
